db06509ae2d81ccd46a4ca51c03d0fcfb2a1c79e
Paweł Bąk <pawel.bak98@gmail.com>
2019-12-09 17:49:13 +0100

    LibraryApp na androida

---
+++ Mobilki/LibraryApp/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+c
+3app/src/main/java/pl/edu/pb/libraryapp/BookDao.java,4/8/4867778b90ffa3e96c7b67c4836a832013a732ef
+j
+:app/src/main/java/pl/edu/pb/libraryapp/BookRepository.java,9/6/96abb78739484a91eaed56f16cf85ed2198e8f02
+i
+9app/src/main/java/pl/edu/pb/libraryapp/BookViewModel.java,a/b/abb30edeab9817c343e1d660fdcd206f6b9f459b
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+`
+0app/src/main/java/pl/edu/pb/libraryapp/Book.java,2/7/27d55b0d646cf04beb3ed4b11eeef9c5f8ff8f49
+h
+8app/src/main/java/pl/edu/pb/libraryapp/BookDatabase.java,e/9/e98fa5f115585188461bab9a3e8f44e8303e5532
+l
+<app/src/main/java/pl/edu/pb/libraryapp/EditBookActivity.java,3/3/338ef083c7297a5c79488a221e8e5b9c6f30a184
+h
+8app/src/main/java/pl/edu/pb/libraryapp/MainActivity.java,1/b/1b1b82a41d47244882491b587f78228142187fe1
---
+++ Mobilki/LibraryApp/app/src/main/java/pl/edu/pb/libraryapp/MainActivity.java
@@ -0,0 +1,175 @@
+package pl.edu.pb.libraryapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private BookViewModel bookViewModel;
+    public static final int NEW_BOOK_ACTIVITY_REQUEST_CODE = 1;
+    public static final int EDIT_BOOK_ACTIVITY_REQUEST_CODE = 2;
+    private Book editBook = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final BookAdapter adapter = new BookAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        bookViewModel = ViewModelProviders.of(this).get(BookViewModel.class);
+        bookViewModel.findAll().observe(this, new Observer<List<Book>>() {
+            @Override
+            public void onChanged(List<Book> books) {
+                adapter.setBooks(books);
+            }
+        });
+
+        FloatingActionButton addBookButton = findViewById(R.id.add_button);
+        addBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditBookActivity.class);
+                startActivityForResult(intent, NEW_BOOK_ACTIVITY_REQUEST_CODE);
+            }
+        });
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_BOOK_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Book book = new Book(data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_TITLE),
+                    data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_AUTHOR));
+            bookViewModel.insert(book);
+            Snackbar.make(findViewById(R.id.coordinator_layout), getString(R.string.book_added),
+                    Snackbar.LENGTH_LONG).show();
+        } else if (requestCode == EDIT_BOOK_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            editBook.setTitle(data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_TITLE));
+            editBook.setAuthor(data.getStringExtra(EditBookActivity.EXTRA_EDIT_BOOK_AUTHOR));
+            bookViewModel.update(editBook);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private class BookHolder extends RecyclerView.ViewHolder {
+        private TextView bookTitleTextView;
+        private TextView bookAuthorTextView;
+
+        public BookHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.book_list_item, parent, false));
+
+            bookTitleTextView = itemView.findViewById(R.id.book_title);
+            bookAuthorTextView = itemView.findViewById(R.id.book_author);
+        }
+
+        public void bind(Book book) {
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    editBook = book;
+                    Intent intent = new Intent(MainActivity.this, EditBookActivity.class);
+                    intent.putExtra(EditBookActivity.EXTRA_EDIT_BOOK_TITLE, book.getTitle());
+                    intent.putExtra(EditBookActivity.EXTRA_EDIT_BOOK_AUTHOR, book.getAuthor());
+                    startActivityForResult(intent, EDIT_BOOK_ACTIVITY_REQUEST_CODE);
+                }
+            });
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    bookViewModel.delete(book);
+                    return true;
+                }
+            });
+            bookTitleTextView.setText(book.getTitle());
+            bookAuthorTextView.setText(book.getAuthor());
+        }
+    }
+
+    private class BookAdapter extends RecyclerView.Adapter<BookHolder> {
+        private List<Book> books;
+
+        @NonNull
+        @Override
+        public BookHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new BookHolder(getLayoutInflater(), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull BookHolder holder, int position) {
+            if (books != null) {
+                Book book = books.get(position);
+                holder.bind(book);
+            } else {
+                Log.d("MainActivity", "No books");
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            if (books != null) {
+                return books.size();
+            } else {
+                return 0;
+            }
+        }
+
+        public void setBooks(List<Book> books) {
+            this.books = books;
+            notifyDataSetChanged();
+        }
+    }
+}
